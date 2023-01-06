fb571e59c2cca6098c281bb2f6df7b7ffa8e3175
mabushelbaia <mabushelbaia@gmail.com>
2023-01-06 13:25:36 +0200

    Final Touches

--- Web Server/app.js
+++
@@ -1,6 +0,0 @@
-const tween = KUTE.fromTo(
-    '#blob1',
-    { path: '#blob1' },
-    { path: '#blob2' },
-    { repeat: 999, duration: 3000, yoyo: true }
-).start();
--- Web Server/server.py
+++ Web Server/server.py
@@ -1,33 +1,25 @@
 from socket import *
+import os
 serverPort = 8080
 server_socket = socket(AF_INET,SOCK_STREAM)
 server_socket.bind(('', serverPort))
 server_socket.listen()
 print("Server is listening on port {}".format(serverPort))
-try:
-    while True:
-        print("Waiting for requests...")
-        connection, address = server_socket.accept()
-        message = connection.recv(1024).decode('utf-8')
-        if not message:
-            break
-        request_method = message.split(" ")[0]
-        request_url = message.split(" ")[1]
-        print(request_method, request_url)
-        if request_method == "GET" and (request_url == "/" or request_url == "/en"):
+def handle_requests(connection, request_method, request_url):
+        if request_method == "GET" and request_url in ["/", "/en", "/index.html", "/main_en.html"]:
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("index.html", "r") as f:
+            with open("main_en.html", "r") as f:
                 connection.send(f.read().encode('utf-8'))
-                print("Sent index.html")
-        elif request_method == "GET" and request_url == "/ar":
+                print("Sent main_en.html")
+        elif request_method == "GET" and request_url in ["ar", "/ar", "/index_ar.html", "/main_ar.html"]:
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("index.html", "r") as f:
+            with open("main_ar.html", "r") as f:
                 connection.send(f.read().encode('utf-8'))
-                print("Sent index.html")
+                print("Sent main_ar.html")
         elif request_method == "GET" and request_url == "/style.css":
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/css\r\n".encode('utf-8'))
@@ -42,20 +34,38 @@ try:
             with open("favicon.ico", "rb") as f:
                 connection.send(f.read())
                 print("Sent favicon.ico")
-        elif request_method == "GET" and request_url == "/images/first_logo.jpg":
+        # Images
+        elif request_method == "GET" and request_url[8:] in os.listdir("images"):
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: image/jpg\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("images/first_logo.jpg", "rb") as f:
+            with open(request_url[1:], "rb") as f:
                 connection.send(f.read())
-                print("Sent first_logo.jpg")
-        elif request_method == "GET" and request_url == "/images/gh.png":
-            connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
-            connection.send("Content-Type: image/jpg\r\n".encode('utf-8'))
+                print("Sent {}.jpg".format(request_url[8:]))
+        elif request_method == "GET" and request_url in ["/go", "/so", "/bzu"]:
+            link = {"/go": "Location: https://www.google.com", "/so": "Location: https://www.stackoverflow.com", "/bzu": "Location: https://www.birzeit.edu"}
+            connection.send("HTTP/1.1 307 Temporary Redirect\r\n".encode('utf-8'))
+            connection.send(f"{link[request_url]}\r\n".encode('utf-8'))
+
+        else:
+            connection.send("HTTP/1.1 404 Not Found\r\n".encode('utf-8'))
+            connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("images/gh.png", "rb") as f:
-                connection.send(f.read())
-                print("Sent first_logo.jpg")
+            with open("not_found.html", "r") as f:
+                text = f.read()
+                text = text.format(ip=address[0], port=address[1])
+                connection.send(text.encode('utf-8'))
+            print("Sent 404 Not Found")
+try:
+    while True:
+        connection, address = server_socket.accept()
+        message = connection.recv(1024).decode('utf-8')
+        print(message)
+        if not message:
+            break
+        request_method = message.split(" ")[0]
+        request_url = message.split(" ")[1]
+        handle_requests(connection, request_method, request_url)
         connection.close()
 except KeyboardInterrupt:
     print("Keyboard interrupt received. Shutting down server.")
