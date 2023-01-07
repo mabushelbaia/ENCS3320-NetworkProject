86219a1ede25046585e3481016ce2e1bca2196ac
mabushelbaia <mabushelbaia@gmail.com>
2023-01-07 20:12:52 +0200

    Finally Finished

--- Web Server/server.py
+++ Web Server/server.py
@@ -1,6 +1,6 @@
 from socket import *
 import os
-serverPort = 7788
+serverPort = 7781
 server_socket = socket(AF_INET,SOCK_STREAM)
 server_socket.bind(('', serverPort))
 server_socket.listen()
@@ -10,21 +10,21 @@ def handle_requests(connection, request_method, request_url):
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("main_en.html", "r") as f:
+            with open("static/main_en.html", "r") as f:
                 connection.send(f.read().encode('utf-8'))
                 print("Sent main_en.html")
         elif request_method == "GET" and request_url in ["ar", "/ar", "/index_ar.html", "/main_ar.html"]:
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("main_ar.html", "r") as f:
+            with open("static/main_ar.html", "r", encoding="utf-8") as f:
                 connection.send(f.read().encode('utf-8'))
                 print("Sent main_ar.html")
         elif request_method == "GET" and request_url == "/style.css":
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             connection.send("Content-Type: text/css\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("style.css", "r") as f:
+            with open("styles/style.css", "r") as f:
                 connection.send(f.read().encode('utf-8'))
                 print("Sent styles.css")
         elif request_method == "GET" and request_url == "/favicon.ico":
@@ -37,21 +37,49 @@ def handle_requests(connection, request_method, request_url):
         # Images
         elif request_method == "GET" and request_url[8:] in os.listdir("images"):
             connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
-            connection.send("Content-Type: image/jpg\r\n".encode('utf-8'))
+            connection.send(f"Content-Type: image/{request_url[-3:]}\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
             with open(request_url[1:], "rb") as f:
                 connection.send(f.read())
-                print("Sent {}.jpg".format(request_url[8:]))
+                print("Sent {}".format(request_url[8:]))
         elif request_method == "GET" and request_url in ["/go", "/so", "/bzu"]:
             link = {"/go": "Location: https://www.google.com", "/so": "Location: https://www.stackoverflow.com", "/bzu": "Location: https://www.birzeit.edu"}
             connection.send("HTTP/1.1 307 Temporary Redirect\r\n".encode('utf-8'))
             connection.send(f"{link[request_url]}\r\n".encode('utf-8'))
-
+        elif request_method == "GET" and request_url == "/ahmadosh_en":
+            connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
+            connection.send("Content-Type: text/html\r\n".encode('utf-8'))
+            connection.send("\r\n".encode('utf-8'))
+            with open("static/project.html", "r", encoding="utf-8") as f:
+                connection.send(f.read().encode('utf-8'))
+                print("Sent project.html.html")
+        elif request_method == "GET" and request_url == "/ahmadosh_ar":
+            connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
+            connection.send("Content-Type: text/html\r\n".encode('utf-8'))
+            connection.send("\r\n".encode('utf-8'))
+            with open("static/project_ar.html", "r", encoding="utf-8") as f:
+                connection.send(f.read().encode('utf-8'))
+                print("Sent project_ar.html")
+        elif request_method == "GET" and request_url == "/project.css":
+            connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
+            connection.send("Content-Type: text/css\r\n".encode('utf-8'))
+            connection.send("\r\n".encode('utf-8'))
+            with open("styles/project.css", "r", encoding="utf-8") as f:
+                connection.send(f.read().encode('utf-8'))
+                print("Sent project.css")
+        elif request_method == "GET" and request_url == "/project.js":
+            connection.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
+            connection.send("Content-Type: text/javascript\r\n".encode('utf-8'))
+            connection.send("\r\n".encode('utf-8'))
+            with open("styles/project.js", "r", encoding="utf-8") as f:
+                connection.send(f.read().encode('utf-8'))
+                print("Sent project.js")
+            
         else:
             connection.send("HTTP/1.1 404 Not Found\r\n".encode('utf-8'))
             connection.send("Content-Type: text/html\r\n".encode('utf-8'))
             connection.send("\r\n".encode('utf-8'))
-            with open("not_found.html", "r") as f:
+            with open("static/not_found.html", "r") as f:
                 text = f.read()
                 text = text.format(ip=address[0], port=address[1])
                 connection.send(text.encode('utf-8'))
